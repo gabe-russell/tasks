9a7c5558c173217fa98bbb37b3f3d44d7fa22211
Gabe Russell <gaberussell@Gabes-MacBook-Pro.local>
2023-09-11 12:47:53 -0400

    fixed shout function

--- src/functions.ts
+++ src/functions.ts
@@ -30,7 +30,7 @@ export function add3(first: number, second: number, third: number): number {
  * mark added to the end.
  */
 export function shout(message: string): string {
-    const uppercase = message.toUpperCase;
+    const uppercase = message.toUpperCase();
     const final = uppercase + "!";
     return final;
 }
