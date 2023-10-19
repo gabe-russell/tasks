08a66b352926b24ac72373b949a52343953dea89
gabe-russell <gabriel.russell@maine.edu>
2023-10-19 17:13:57 -0400

    finished first component errors

--- src/App.tsx
+++ src/App.tsx
@@ -19,7 +19,7 @@ function App(): JSX.Element {
             </header>
             <p>Some info</p>
             <hr></hr>
-            {/* <DoubleHalf></DoubleHalf> */}
+            {<DoubleHalf></DoubleHalf>}
             <hr></hr>
             <ChooseTeam></ChooseTeam>
             <hr></hr>
--- src/bad-components/DoubleHalf.tsx
+++ src/bad-components/DoubleHalf.tsx
@@ -1,24 +1,37 @@
 import React, { useState } from "react";
 import { Button } from "react-bootstrap";
-import { dhValue, setDhValue } from "./DoubleHalfState";
+//import { dhValue, setDhValue } from "./DoubleHalfState";
 
-function Doubler(): JSX.Element {
-    return <Button onClick={() => setDhValue(2 * dhValue)}>Double</Button>;
+function Doubler({
+    currentValue,
+    onDouble
+}: {
+    currentValue: number;
+    onDouble: (value: number) => void;
+}): JSX.Element {
+    return <Button onClick={() => onDouble(2 * currentValue)}>Double</Button>;
 }
 
-function Halver(): JSX.Element {
-    return <Button onClick={() => setDhValue(0.5 * dhValue)}>Halve</Button>;
+function Halver({
+    currentValue,
+    onHalve
+}: {
+    currentValue: number;
+    onHalve: (value: number) => void;
+}): JSX.Element {
+    return <Button onClick={() => onHalve(0.5 * currentValue)}>Halve</Button>;
 }
 
 export function DoubleHalf(): JSX.Element {
+    const [dhValue, setDhValue] = useState<number>(10);
     return (
         <div>
             <h3>Double Half</h3>
             <div>
                 The current value is: <span>{dhValue}</span>
             </div>
-            <Doubler></Doubler>
-            <Halver></Halver>
+            <Doubler currentValue={dhValue} onDouble={setDhValue} />
+            <Halver currentValue={dhValue} onHalve={setDhValue} />
         </div>
     );
 }
