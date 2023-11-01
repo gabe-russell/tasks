0ec563c2ee6e0405f932f2154079b6182b2a0dd1
gabe-russell <gabriel.russell@maine.edu>
2023-11-01 18:59:39 -0400

    finished CheckAnswer and GiveAttempts

--- src/form-components/CheckAnswer.tsx
+++ src/form-components/CheckAnswer.tsx
@@ -1,13 +1,23 @@
 import React, { useState } from "react";
+import { Form } from "react-bootstrap";
 
 export function CheckAnswer({
     expectedAnswer
 }: {
     expectedAnswer: string;
 }): JSX.Element {
+    const [answer, setAnswer] = useState<string>("");
+
+    function updateAnswer(event: React.ChangeEvent<HTMLInputElement>) {
+        setAnswer(event.target.value);
+    }
     return (
         <div>
-            <h3>Check Answer</h3>
+            <Form.Group controlId="formAnswer">
+                <Form.Label>Type Answer</Form.Label>
+                <Form.Control value={answer} onChange={updateAnswer} />
+            </Form.Group>
+            <h3>The answer is {answer === expectedAnswer ? "✔️" : "❌"}</h3>
         </div>
     );
 }
