e11a46d14ee639f7c95df1c46075832196ce9559
gabe-russell <gabriel.russell@maine.edu>
2023-11-01 19:53:14 -0400

    finished ChangeColor and MultipleChoiceQuestion

--- src/form-components/MultipleChoiceQuestion.tsx
+++ src/form-components/MultipleChoiceQuestion.tsx
@@ -1,4 +1,5 @@
 import React, { useState } from "react";
+import { Form } from "react-bootstrap";
 
 export function MultipleChoiceQuestion({
     options,
@@ -7,9 +8,27 @@ export function MultipleChoiceQuestion({
     options: string[];
     expectedAnswer: string;
 }): JSX.Element {
+    const [choice, setChoice] = useState<string>(options[0]);
+
+    function updateChoice(event: React.ChangeEvent<HTMLSelectElement>) {
+        setChoice(event.target.value);
+    }
     return (
         <div>
-            <h3>Multiple Choice Question</h3>
+            <Form.Group controlId="dropdownQuiz">
+                <Form.Label>Multiple Choice</Form.Label>
+                <Form.Select value={choice} onChange={updateChoice}>
+                    {options.map((selection: string) => (
+                        <option key={selection} value={selection}>
+                            {selection}
+                        </option>
+                    ))}
+                </Form.Select>
+            </Form.Group>
+            <h3>
+                The Answer is
+                {choice === expectedAnswer ? "✔️" : "❌"}
+            </h3>
         </div>
     );
 }
